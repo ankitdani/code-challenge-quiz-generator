1f889a72d87ef383c0b08dab14db86a0891d134c
unknown <ankitdani1997@gmail.com>
2025-08-02 12:52:40 -0700

    Add authentication page

--- frontend/src/auth/AuthenticationPage.jsx
+++ frontend/src/auth/AuthenticationPage.jsx
@@ -1,9 +1,19 @@
 import "react"
+import {SignIn, SignUp, SignedIn, SignedOut} from "@clerk/clerk-react"
 
 export default function AuthenticationPage() {
     return (
-        <div>
-            <h1>Authentication Page</h1>
+        <div className="auth-container">
+            <SignedOut>
+                <SignIn routing="path" path="/sign-in" />
+                <SignUp routing="path" path="/sign-up" />
+            </SignedOut>
+            <SignedIn>
+                <div className="redirect-message">
+                    <p>You are already signed in.</p>
+                </div>
+            </SignedIn>
+            
         </div>
     )
 }
