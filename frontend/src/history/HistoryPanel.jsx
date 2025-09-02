fc68f588ee02bd2d35a99b468a9a7f845f49a4fa
unknown <ankitdani1997@gmail.com>
2025-09-01 21:26:56 -0700

    Add challenge history

--- frontend/src/history/HistoryPanel.jsx
+++ frontend/src/history/HistoryPanel.jsx
@@ -1,44 +1,63 @@
-import "react"
-import { useState, useEffect } from "react"
-import MCQChallenge from "../challenge/MCQChallenge.jsx"
+import "react";
+import { useState, useEffect } from "react";
+import MCQChallenge from "../challenge/MCQChallenge.jsx";
+import { useApi } from "../utils/api.js";
 
 export default function HistoryPanel() {
-    const [history, setHistory] = useState([]);
-    const [isLoading, setIsLoading] = useState(true);
-    const [error, setError] = useState(null);
+  const { makeRequest } = useApi();
+  const [history, setHistory] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
+  const [error, setError] = useState(null);
 
-    useEffect(()=>{
-        fetchHistory();
-    }, [])
+  useEffect(() => {
+    fetchHistory();
+  }, []);
 
-    const fetchHistory = async () => {
-        setIsLoading(false);
+  const fetchHistory = async () => {
+    setIsLoading(true);
+    setError(null);
+
+    try {
+      const data = await makeRequest("my-history");
+      setHistory(data.challenges);
+    } catch (err) {
+      setError("Failed to load history");
+    } finally {
+      setIsLoading(false);
     }
+  };
 
-    if (isLoading){
-        return <div>loading....</div>
-    }
+  if (isLoading) {
+    return <div>loading....</div>;
+  }
 
-    if (error){
-        return <div className="error-message">
-            <p>{error}</p>
-            <button onClick={fetchHistory}>Retry</button>
-        </div>
-    }
-
+  if (error) {
     return (
-        <div className="history-panel">
-            <h2>History</h2>
-            {history.length === 0 ? <p>No challenge history</p> : 
-            <div className="history-list">
-                {history.map((challenge) => {
-                    return <MCQChallenge 
-                            challenge={challenge}
-                            key = {challenge.id}
-                            showExplainination
-                    />
-                })}
-            </div>}
+      <div className="error-message">
+        <p>{error}</p>
+        <button onClick={fetchHistory}>Retry</button>
+      </div>
+    );
+  }
+
+  return (
+    <div className="history-panel">
+      <h2>History</h2>
+      {history.length === 0 ? (
+        <p>No challenge history</p>
+      ) : (
+        <div className="history-list">
+          {history.map((challenge) => {
+            return (
+              <MCQChallenge
+                challenge={challenge}
+                key={challenge.id}
+                showExplanation
+              />
+            );
+          })}
         </div>
-    )
+      )}
+    </div>
+  );
 }
